aba7917c42ca73c8837dc68bad84bcde1e242674
gparatcha <gparatcha@gmail.com>
2023-04-27 13:31:41 -0300

    Add loading to UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
-import { Col } from 'antd';
+import { Col, Spin } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import { getPokemon, getPokemonDetails } from './api';
 //import { setPokemons as setPokemonsActions } from './actions';
-import { getPokemonsWithDetails } from './actions';
+import { getPokemonsWithDetails, setLoading } from './actions';
 //import { connect } from 'react-redux';
 import logo from './statics/logo.svg';
 import './App.css';
@@ -15,10 +15,12 @@ import './App.css';
 function App() {
 
   const pokemons = useSelector(state => state.pokemons);
+  const loading = useSelector(state => state.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemons = async () => {
+      dispatch(setLoading(true));
       const pokemonsRes = await getPokemon();
       
       /*const pokemonsDetailed = await Promise.all(pokemonsRes.map(pokemon =>
@@ -26,6 +28,7 @@ function App() {
 
     //  dispatch(setPokemons(pokemonsRes));
       dispatch(getPokemonsWithDetails(pokemonsRes));
+      dispatch(setLoading(false));
     };
 
     
@@ -40,7 +43,13 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons}/>
+      {
+      loading ? (
+        <Col offset={12}>
+          <Spin spinning size='large' />
+        </Col>
+      ) : (<PokemonList pokemons={pokemons}/>)
+      }
     </div>
   );
 }
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
-import { SET_POKEMONS } from "./types";
+import { SET_LOADING, SET_POKEMONS } from "./types";
 import { getPokemonDetails } from "../api";
 
 export const setPokemons = (payload) => ({
     type: SET_POKEMONS,
     payload,
 });
+
+export const setLoading = (payload) => ({
+    type: SET_LOADING,
+    payload,
+});
+
+/* THIS IS WITH REDUX THUNK: */
 export const getPokemonsWithDetails = (pokemons = []) => 
     async (dispatch) => {
         const pokemonsDetailed = await Promise.all(
